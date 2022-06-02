44b0b33aa46826bf28bf915cbbba76298e25f233
eslam ahmed <eslam.ahmed@hitop.com.sa>
2022-06-02 15:32:46 +0200

    implementing ui and static data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task';
+  public aufgaben: Observable<any>[] = [];
+
+  constructor(afDb: AngularFireDatabase) {
+    const itemsRef: AngularFireList<any> = afDb.list('Aufgaben');
+    itemsRef.valueChanges().subscribe(
+      x => { this.aufgaben = x; }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AngularFireModule } from 'angularfire2';
+// import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig={
+  apiKey:"AIzaSyCYCRSPByfE X201SVDc@z_MQ10JVKYZvM",
+  authDomain:"dailycheck-d9983.firebaseapp.com",
+  databaseURL:"https://dailycheck-d9983-default-rtdb.europe-west1.firebasedatabase.app",
+  projectid:"dailycheck-d9983",
+  storageBucket:"dailycheck-d9983.appspot.com",
+  messagingSenderId:"594584322163",
+  appId:"1:594584322163:web:acff93d09af6338faaaa06",
+  measurementId:"G-PB9RFL53XQ" ,
+  measuementId:"https://dailycheck-d9983-default-rtdb.europe-westl.firebasedatabase.app"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule , 
+    AngularFireModule.initializeApp(firebaseConfig) ,
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
